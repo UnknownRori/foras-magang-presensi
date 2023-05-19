924e12fe1219c27e88a0d3220976ea0ed08344f8
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-05-20 06:22:23 +0700

    🚀 Implement relationship on model

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,13 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -42,4 +46,21 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn (string $plain) => Hash::make($plain),
+        );
+    }
+
+    public function checkout(): HasMany
+    {
+        return $this->hasMany(Checkout::class);
+    }
+
+    public function checkin(): HasMany
+    {
+        return $this->hasMany(CheckIn::class);
+    }
 }
