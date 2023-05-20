b45be7dd59c592560aec6f122f490a2811fef730
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-05-20 19:18:44 +0700

    📦 Create basic welcome page

---
+++ resources/views/components/shared/base.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="{{ asset('assets/img/foras.png') }}" type="image/x-icon">
+    <title>Sistem Presensi Foras</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="h-[94vh]">
+
+    <body>
+        {{ $slot }}
+    </body>
+</body>
+
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,27 @@
-<!doctype html>
-<html>
+@component('components.shared.base')
+    <div class="absolute z-10 flex w-[100%] flex-row-reverse px-6 py-2">
+        <div class="flex flex-row-reverse">
+            <a href="{{ route('login') }}" class="md:text-md flex text-center text-gray-600 lg:text-lg">Login</a>
+        </div>
+    </div>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
-</head>
+    <div class="relative flex min-h-[100%] flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
+        <div
+            class="absolute inset-0 bg-[url({{ asset('assets/img/grid.svg') }})] bg-center bg-repeat [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]">
+        </div>
 
-<body>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-</body>
-
-</html>
+        <div
+            class="relative w-[60rem] bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-5xl sm:rounded-lg sm:px-10">
+            <div class="mx-auto">
+                <div class="flex justify-center">
+                    <img src="{{ asset('assets/img/foras.png') }}" class="h-48 rounded-full" alt="Tailwind Play" />
+                </div>
+                <div class="divide-y divide-gray-300/50">
+                    <div class="space-y-6 py-8 text-base leading-7 text-gray-600">
+                        <h3 align="center" class="text-3xl">Sistem Presensi</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endcomponent
