348087c060fce87fbd71b464e8fdb063fed4793a
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-05-21 10:43:08 +0700

    📦 Added min:4 in name

--- app/Http/Requests/LoginRequest.php
+++ app/Http/Requests/LoginRequest.php
@@ -22,7 +22,7 @@ class LoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required'],
+            'name' => ['required', 'min:4'],
             'password' => ['required'],
         ];
     }
