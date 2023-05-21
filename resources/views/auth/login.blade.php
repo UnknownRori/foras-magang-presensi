68c1c48b82b9da83200b07b8a75fa5acc1d9a3df
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-05-21 10:43:35 +0700

    🛠️ Some minor fix

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,9 +2,17 @@
     <div class="align-items-center flex min-h-full min-w-full justify-center px-6 py-2">
         <div class="flex w-[32vw] flex-col gap-2 self-center rounded-md border-2 border-gray-300 p-4 shadow-md">
             <img src="{{ asset('assets/img/foras.webp') }}" class="h-32 self-center" alt="Foras">
-            <form action="{{ route('post-login') }}" class="flex flex-col gap-4">
-                <x-shared.inputform id="name" type="text" placeholder="Masukan nama anda" />
-                <x-shared.inputform id="password" type="password" placeholder="Masukan password anda" />
+            <form action="{{ route('post-login') }}" class="flex flex-col gap-4" method="post">
+                @csrf
+                <x-shared.inputform :value="old('name')" id="name" type="text" placeholder="Masukan nama anda"
+                    msg="Nama harus di-isi dan lebih dari 4" />
+                <x-shared.inputform id="password" type="password" placeholder="Masukan password anda"
+                    msg="Password harap di-isi" />
+                @if (session()->has('message'))
+                    <x-alert-label>
+                        {{ session()->get('message') }}
+                    </x-alert-label>
+                @endif
                 <div>
                     <input type="submit" value="Login"
                         class="cursor-pointer rounded-md border-blue-400 bg-blue-500 p-2 text-white hover:bg-blue-700">
