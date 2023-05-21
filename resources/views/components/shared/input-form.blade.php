e63a911927ff164ccebb6460be3745fe1a447885
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-05-21 19:39:57 +0700

    📦 Extend the error message and allow customization

--- resources/views/components/shared/input-form.blade.php
+++ resources/views/components/shared/input-form.blade.php
@@ -1,11 +1,18 @@
 <div class="flex flex-col gap-2">
-    <label for="{{ $id }}" class="text-slate-800">{{ str($id)->ucfirst() }}</label>
+    <label for="{{ $id }}" class="text-slate-800">{{ isset($title) ? $title : str($id)->ucfirst() }}</label>
     <input type="{{ $type }}" name="{{ $id }}" id="{{ $id }}" placeholder="{{ $placeholder }}"
-        class="rounded-md border-2 border-gray-400 p-2" value="{{ $value }}" required>
+        class="rounded-md border-2 border-gray-400 p-2" value="{{ $value }}"
+        {{ $isNotRequired ? '' : 'required' }}>
     @if ($attributes->has('msg'))
         @error($id)
             <x-shared.alert>
                 {{ $attributes->get('msg') }}
+            </x-shared.alert>
+        @enderror
+    @else
+        @error($id)
+            <x-shared.alert>
+                {{ $errors->first($id) }}
             </x-shared.alert>
         @enderror
     @endif
