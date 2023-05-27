5cc4dd605975afb043599085b521fda05b6c24ef
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-05-28 04:59:28 +0700

    📦 Added Role Selection

--- resources/views/dashboard/user/edit.blade.php
+++ resources/views/dashboard/user/edit.blade.php
@@ -11,10 +11,16 @@
                 msg="Nama harus di-isi dan lebih dari 4" />
             <x-shared.inputform id="old-password" title="Password Lama" type="password"
                 placeholder="Masukan password lama" msg="Password Wajib di-isi" />
-            <x-shared.inputform id="password" type="password" placeholder="Masukan password user"
-                msg="Password minimal 4 karakter" :isNotRequired='true' />
-            <x-shared.inputform id="password_confirmation" title="Konfirmasi Password" type="password"
-                placeholder="Konfirmasi password user" msg="Password minimal 4 karakter" :isNotRequired='true' />
+            @admin
+                <select name="role" id="role">
+                    <option value="Admin">Admin</option>
+                    <option value="NonAdmin">NonAdmin</option>
+                </select>
+            @endadmin
+            <x-shared.inputform id="password" type="password" placeholder="Masukan password baru user"
+                msg="Password minimal 4 karakter" title="Password Baru" :isNotRequired='true' />
+            <x-shared.inputform id="password_confirmation" title="Konfirmasi Password Baru" type="password"
+                placeholder="Konfirmasi password baru user" msg="Password minimal 4 karakter" :isNotRequired='true' />
 
             <x-shared.auto-alert />
             <div>
