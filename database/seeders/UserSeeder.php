e16063bac7e1738619abdefb4e6c5b76bc0a1a8c
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-10 06:27:53 +0700

    🛠️ Fix seeder to correspond with latest migration

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\RoleEnum;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -18,25 +19,28 @@ class UserSeeder extends Seeder
             'name' => $name,
             'password' => $name,
             'email' => 'unknownrori@mail.test',
-            'admin' => 1
+            'role' => RoleEnum::Admin->name,
         ]);
 
         $name = 'rigit';
         User::create([
             'name' => $name,
             'password' => $name,
+            'role' => RoleEnum::Magang->name,
         ]);
 
         $name = 'pingkan';
         User::create([
             'name' => $name,
             'password' => $name,
+            'role' => RoleEnum::Magang->name,
         ]);
 
         $name = 'risti';
         User::create([
             'name' => $name,
             'password' => $name,
+            'role' => RoleEnum::Magang->name,
         ]);
     }
 }
