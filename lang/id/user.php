5201718e42f583c06850276b2b59a33f8b5edec1
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-10 07:28:09 +0700

    📦 Added another translation file

---
+++ lang/id/user.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'edit-success' => 'User berhasil diedit',
+    'edit-failed' => 'User gagal diedit',
+    'delete-success' => 'User berhasil dihapus',
+    'delete-failed' => 'User gagal dihapus',
+];
+
